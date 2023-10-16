dc4456703196bda7df8f36522370a531b1d724d0
Aline <alinevitoriadesouza0@gmail.com>
2023-10-15 21:28:24 -0300

    Adicionando classe clientes

---
+++ src/entities/cliente.js
@@ -0,0 +1,9 @@
+export default class Clientes{
+    constructor(id_cliente, nome, documento, email, senha){
+        this.id_cliente
+        this.nome
+        this.documento
+        this.email
+        this.senha
+    }
+}
