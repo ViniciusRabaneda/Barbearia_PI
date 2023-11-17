e7561e414caa32e9f76bf024cb4f0dd61147d8ee
Anthony <anthony.oliveira5@hotmail.com>
2023-11-17 18:20:01 -0300

    Finalizando as verificações necessárias na rota serviço

--- src/routes/services.js
+++ src/routes/services.js
@@ -25,18 +25,30 @@ export default class ServiceRoutes {
     router.post('/', (req, res) => {
       const novoServico = req.body
 
-      console.log('novoServico.id:', novoServico.id);
-      console.log('this.db.servico:', this.db.findById(novoServico.id));
+      /* console.log(typeof(novoServico.corteBarba))
+      console.log(typeof(novoServico.barba))
+      console.log(typeof(novoServico.corteCabelo))
+      console.log(typeof(novoServico.sobrancelha))
+      console.log(typeof(novoServico.corteSobrancelha))
+      console.log(typeof(novoServico.corteBarbaSobrancelha))
+      console.log(typeof(novoServico.pigmentacao))
+      console.log(typeof(novoServico.undercutFem)) */
 
       //verifica se o id do serviço já está cadastrado ou não no sistema
       if(this.db.findById(novoServico.id) != undefined) return res.status(400).json({message: 'Este id já está cadastrado no sistema.'})
+      //verifica se foi preenchido o id
       if(!novoServico.id) return res.status(400).json({ message: 'O id é obrigatório' })
+      //verifica se foi colocado valor diferente de numero no id
       if(!Number(novoServico.id)) return res.status(400).json({message: 'O id é composto apenas de números.'})
+      //verifica se foi preenchido o id cliente
       if(!novoServico.idCliente) return res.status(400).json({ message: 'O campo idCliente é obrigatório' })
+      //verifica se foi colocado valor diferente de numero no id cliente
       if(!Number(novoServico.idCliente)) return res.status(400).json({message: 'O idCliente é composto apenas de números.'})
-      if(novoServico.corteCabelo & novoServico.barba & novoServico.sobrancelha & novoServico.corteBarba & novoServico.corteSobrancelha & novoServico.corteBarbaSobrancelha & novoServico.pigmentacao & novoServico.undercutFem == false) return res.status(400).json({ message: 'É necessário solicitar ao menos um serviço para continuar.' })
-      /* if(!Boolean(novoServico.corteCabelo & novoServico.barba & novoServico.sobrancelha & novoServico.corteBarba & novoServico.corteSobrancelha & novoServico.corteBarbaSobrancelha & novoServico.pigmentacao & novoServico.undercutFem)) return res.status(400).json({ message: 'Os valores preenchidos só podem ser verdadeiro ou falso.' }), console.log(novoServico) */
-      //comentado pois ainda não está funcionando
+      //verifica se todos os valores são false, se for, não foi solicitado nenhum serviço
+      if(novoServico.corteCabelo !== true && novoServico.barba !== true &&  novoServico.sobrancelha !== true && novoServico.corteBarba !== true && novoServico.corteSobrancelha !== true &&  novoServico.corteBarbaSobrancelha !== true &&  novoServico.pigmentacao !== true &&  novoServico.undercutFem !== true) return res.status(400).json({ message: 'É necessário solicitar ao menos um serviço para continuar.' })
+      //verifica se foi colocado valor diferente de verdadeiro ou falso
+      if(typeof(novoServico.corteCabelo) !== 'boolean' || typeof(novoServico.barba) !== 'boolean' || typeof(novoServico.sobrancelha) !== 'boolean' || typeof(novoServico.corteBarba) !== 'boolean' || typeof(novoServico.corteSobrancelha) !== 'boolean'||(typeof novoServico.corteBarbaSobrancelha) !== 'boolean' || typeof(novoServico.pigmentacao) !== 'boolean' || typeof(novoServico.undercutFem) !== 'boolean') return res.status(400).json({ message: 'Os valores preenchidos só podem ser verdadeiro ou falso.' })
+
       this.db.create(novoServico)
       res.json(novoServico)
     })
@@ -46,12 +58,17 @@ export default class ServiceRoutes {
       const servico = req.body
 
       if(!servico.id) return res.status(400).json({ message: 'O id é obrigatório' })
+
       if(!Number(servico.id)) return res.status(400).json({message: 'O id é composto apenas de números.'})
+
       if(!servico.idCliente) return res.status(400).json({ message: 'O campo idCliente é obrigatório' })
+
       if(!Number(servico.idCliente)) return res.status(400).json({message: 'O idCliente é composto apenas de números.'})
-      if(servico.corteCabelo & servico.barba & servico.sobrancelha & servico.corteBarba & servico.corteSobrancelha & servico.corteBarbaSobrancelha & servico.pigmentacao & servico.undercutFem == false) return res.status(400).json({ message: 'É necessário solicitar ao menos um serviço para continuar.' })
-      /* if(!Boolean(servico.corteCabelo, servico.barba, servico.sobrancelha, servico.corteBarba, servico.corteSobrancelha, servico.corteBarbaSobrancelha, servico.pigmentacao, servico.undercutFem)) return res.status(400).json({ message: 'Os valores preenchidos só podem ser verdadeiro ou falso.' }), console.log(servico) */
-      //comentado pois ainda não está funcionando
+
+      if(novoServico.corteCabelo !== true && novoServico.barba !== true &&  novoServico.sobrancelha !== true && novoServico.corteBarba !== true && novoServico.corteSobrancelha !== true &&  novoServico.corteBarbaSobrancelha !== true &&  novoServico.pigmentacao !== true &&  novoServico.undercutFem !== true) return res.status(400).json({ message: 'É necessário solicitar ao menos um serviço para continuar.' })
+
+      if(typeof(novoServico.corteCabelo) !== 'boolean' || typeof(novoServico.barba) !== 'boolean' || typeof(novoServico.sobrancelha) !== 'boolean' || typeof(novoServico.corteBarba) !== 'boolean' || typeof(novoServico.corteSobrancelha) !== 'boolean'||(typeof novoServico.corteBarbaSobrancelha) !== 'boolean' || typeof(novoServico.pigmentacao) !== 'boolean' || typeof(novoServico.undercutFem) !== 'boolean') return res.status(400).json({ message: 'Os valores preenchidos só podem ser verdadeiro ou falso.' })
+
       this.db.update(id, servico)
       res.json(servico)
 
